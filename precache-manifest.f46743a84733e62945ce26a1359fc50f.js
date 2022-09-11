236e1ee6d4ec58e2d233b672d4311eb4323c1e06
RohanSKarthikeyan <rohankarthikeyan7@gmail.com>
2022-09-11 11:06:53 +0530

    Updates

---
+++ precache-manifest.f46743a84733e62945ce26a1359fc50f.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "90976f04b44c3a7bf161",
+    "url": "/dualkart/static/css/main.6acf2462.chunk.css"
+  },
+  {
+    "revision": "90976f04b44c3a7bf161",
+    "url": "/dualkart/static/js/main.90976f04.chunk.js"
+  },
+  {
+    "revision": "f69cd6e071770cb426f2",
+    "url": "/dualkart/static/js/1.f69cd6e0.chunk.js"
+  },
+  {
+    "revision": "36ae2c9b991ea3de5bae",
+    "url": "/dualkart/static/js/runtime~main.36ae2c9b.js"
+  },
+  {
+    "revision": "fb91101b5eb3ecdf6ee2fe361b2fd85d",
+    "url": "/dualkart/static/media/checkout.fb91101b.jpg"
+  },
+  {
+    "revision": "17d305eac5af827fcd84fed0f7ea5cd5",
+    "url": "/dualkart/static/media/Fradel & Spies black.17d305ea.png"
+  },
+  {
+    "revision": "88bd34df6b1371544358db821c51160c",
+    "url": "/dualkart/static/media/Fradel & Spies.88bd34df.png"
+  },
+  {
+    "revision": "e2c8828c3082312aa4c6f4021a2c71e9",
+    "url": "/dualkart/static/media/Amazon_logo.e2c8828c.png"
+  },
+  {
+    "revision": "16934a6e0432054bf317afd26e50dd35",
+    "url": "/dualkart/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/dualkart/precache-manifest.f46743a84733e62945ce26a1359fc50f.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/dualkart/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
+});
